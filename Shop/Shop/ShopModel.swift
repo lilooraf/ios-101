61290ddd51bae53350069a6284f43aca824b2cdc
Raphael Hoareau <lilooraf35@gmail.com>
2021-12-17 15:20:04 -0500

    ShopModel + UnitTests

--- Shop/Shop/ShopModel.swift
+++ Shop/Shop/ShopModel.swift
@@ -1,12 +1,16 @@
 import Foundation
 
-// TODO: Create the data models
-// Check out the API specs on Shop.json 
-
 struct Shop: Codable {
-
+    
+    var company_name: String
+    var products: [Product]
 }
 
 struct Product: Codable {
-    
+
+    var id: String
+    var name: String
+    var image: String
+    var price_cents: Int
+    var currency: String
 }
